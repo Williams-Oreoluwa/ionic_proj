cd35fcc983901fe1e6ce77f47efacca41d710dc4
Your Name <you@example.com>
2023-06-24 10:28:38 +0100

    my-commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Github_Users_with_ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
